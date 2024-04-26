7e40f109df8eab404b35641a64879866c1b7d96b
praiseekoja <preizche@gmail.com>
2024-04-26 03:00:09 +0100

    first commit

---
+++ simple_form_validation/index.php
@@ -0,0 +1,30 @@
+//Implement custom validation rules such as uniqueness checks for email/username,
+// password strength requirements, etc.
+<?php
+if(isset($_POST['submit'])){
+    
+
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="names">
+        <input type="email" name="email">
+        <input type="phone" name="phone">
+        <input type="password" name="password">
+        <input type="password" name="confirm_password">
+        <input type="submit" value="submit">
+
+    </form>
+</body>
+</html>
